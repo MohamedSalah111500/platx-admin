5aadffb281abde4a4b664b9b76ac9300c6be538f
MohamedSalah111500 <mohamedsalah111500@gmail.com>
2025-03-30 05:38:27 +0300

    fix https in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL:'http://platx.runasp.net/',
+  apiURL:'https://platx.runasp.net/',
   defaultauth: 'fakebackend',
   firebaseConfig: {
     apiKey: '',
