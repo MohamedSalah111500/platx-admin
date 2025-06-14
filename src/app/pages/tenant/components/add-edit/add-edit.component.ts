78068c32c3e5efeb49b8340f7cbd24859b2a23ee
Mark Magdy <mark.magdy@cyshield.com>
2025-06-14 11:29:50 +0300

    fix tenant form required fields

--- src/app/pages/tenant/components/add-edit/add-edit.component.ts
+++ src/app/pages/tenant/components/add-edit/add-edit.component.ts
@@ -1,16 +1,16 @@
-import {Component} from "@angular/core";
-import {TenantFormGroup} from "../../types";
+import { Component } from "@angular/core";
+import { TenantFormGroup } from "../../types";
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from "@angular/forms";
-import {ToastrService} from "ngx-toastr";
-import {TenantService} from "../../services/tenantService.service";
-import {errorMapper} from "src/app/utiltis/functions";
-import {Tenant} from "./../../types";
-import {Router} from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { TenantService } from "../../services/tenantService.service";
+import { errorMapper } from "src/app/utiltis/functions";
+import { Tenant } from "./../../types";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "platx-admin-add-edit",
@@ -20,36 +20,36 @@ import {Router} from "@angular/router";
 export class AddEditComponent {
   errorMapper = errorMapper;
   breadCrumbItems: Array<{}> = [
-    {label: "Manage Tenant"},
-    {label: "Create",active: true},
+    { label: "Manage Tenant" },
+    { label: "Create", active: true },
   ];
   submitted = false;
   mode: string = "create";
 
   tenantForm: FormGroup<TenantFormGroup> = this.fb.group<TenantFormGroup>({
     id: new FormControl(null),
-    LastName: new FormControl("",[Validators.required]),
-    FirstName: new FormControl("",[Validators.required]),
-    Domain: new FormControl("",[Validators.required]),
-    PhoneNumber: new FormControl("",[Validators.required]),
-    LogoFile: new FormControl("",[Validators.required]),
-    IsActive: new FormControl(true,[Validators.required]),
-    CoverFile: new FormControl("",[Validators.required]),
-    Title: new FormControl("",[Validators.required]),
-    Email: new FormControl("",[Validators.required]),
+    LastName: new FormControl("", [Validators.required]),
+    FirstName: new FormControl("", [Validators.required]),
+    Domain: new FormControl("", [Validators.required]),
+    PhoneNumber: new FormControl("", [Validators.required]),
+    LogoFile: new FormControl("", []),
+    IsActive: new FormControl(true, [Validators.required]),
+    CoverFile: new FormControl("", []),
+    Title: new FormControl("", [Validators.required]),
+    Email: new FormControl("", [Validators.required]),
     Description: new FormControl(""),
     CreatedBy: new FormControl(""),
-    QuotaAI: new FormControl(30,[Validators.required]),
+    QuotaAI: new FormControl(30, [Validators.required]),
   });
 
-  constructor (
+  constructor(
     private fb: FormBuilder,
     private tenantService: TenantService,
     public toastr: ToastrService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit () {
+  ngOnInit() {
     this.mode = history.state?.mode;
 
     if (this.mode == "edit") {
@@ -57,17 +57,17 @@ export class AddEditComponent {
     }
   }
 
-  getTenant (id: string) {
+  getTenant(id: string) {
     this.tenantService.getTenant(id).subscribe(
       (response) => {
         this.updateForm(response);
       },
-      (error) => { }
+      (error) => {}
     );
   }
 
-  updateForm (tenant: Tenant): void {
-    console.log(tenant)
+  updateForm(tenant: Tenant): void {
+    console.log(tenant);
     const mappedResponse = {
       id: tenant.id,
       LastName: tenant.lastName || "",
@@ -81,48 +81,49 @@ export class AddEditComponent {
       Description: tenant.description || "",
       PhoneNumber: tenant.phoneNumber || "",
       CreatedBy: tenant.createdBy || "",
-      QuotaAI: tenant.quotaAI|| 30,
+      QuotaAI: tenant.quotaAI || 30,
     };
     this.tenantForm.patchValue(mappedResponse);
   }
 
-  onFileUploadSuccess (controlName: string,file: any): void {
+  onFileUploadSuccess(controlName: string, file: any): void {
     this.tenantForm.get(controlName)?.setValue(file);
   }
 
-  onSubmit (): void {
+  onSubmit(): void {
     if (!this.tenantForm.valid) return;
     this.submitted = true;
 
     let formVal = this.tenantForm.value;
     const formData = new FormData();
-    formData.append("CoverFile",formVal.CoverFile);
-    formData.append("CreatedBy",formVal.CreatedBy); //@TODO
-    formData.append("Description",formVal.Description);
-    formData.append("PhoneNumber",formVal.PhoneNumber);
-    formData.append("Domain",formVal.Domain);
-    formData.append("Email",formVal.Email);
-    formData.append("FirstName",formVal.FirstName);
-    formData.append("IsActive",formVal.IsActive.toString());
-    formData.append("LastName",formVal.LastName);
-    formData.append("LogoFile",formVal.LogoFile);
-    formData.append("Title",formVal.Title);
-    formData.append("QuotaAI",formVal.QuotaAI);
+    formData.append("CoverFile", formVal.CoverFile);
+    formData.append("CreatedBy", formVal.CreatedBy); //@TODO
+    formData.append("Description", formVal.Description);
+    formData.append("PhoneNumber", formVal.PhoneNumber);
+    formData.append("Domain", formVal.Domain);
+    formData.append("Email", formVal.Email);
+    formData.append("FirstName", formVal.FirstName);
+    formData.append("IsActive", formVal.IsActive.toString());
+    formData.append("LastName", formVal.LastName);
+    formData.append("LogoFile", formVal.LogoFile);
+    formData.append("Title", formVal.Title);
+    formData.append("QuotaAI", formVal.QuotaAI);
 
     if (this.mode == "edit") {
-      formData.append("Id",formVal.id);
+      formData.append("Id", formVal.id);
       this.tenantService.putUpdateTenant(formData).subscribe({
         next: (response) => {
           this.toastr.success("Tenant updated successfully");
           this.router.navigate(["/tenant"]);
         },
         error: (err) => {
-          this.toastr.error(this.errorMapper(err.error?.errors) ?? "Error Please Try Again");
-          this.submitted = false
-        }
-      }
+          this.submitted = false;
 
-      );
+          this.toastr.error(
+            this.errorMapper(err.error?.errors) ?? "Error Please Try Again"
+          );
+        },
+      });
     } else {
       this.tenantService.postCreateTenant(formData).subscribe(
         (response) => {
@@ -130,8 +131,11 @@ export class AddEditComponent {
           this.router.navigate(["/tenant"]);
         },
         (err) => {
-          this.toastr.error(this.errorMapper(err.error?.errors) ?? "Error Please Try Again");
-          this.submitted = false
+          this.submitted = false;
+
+          this.toastr.error(
+            this.errorMapper(err.error?.errors) ?? "Error Please Try Again"
+          );
         }
       );
     }
